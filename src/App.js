c92b76eafd2caae2832ce5511d42f852a4e5215c
justcallmebeef <bethanymitch10@gmail.com>
2018-11-21 23:29:31 -0700

    added a Home and Footer component along with paralax styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import Home from './components/Home'
+import Footer from './components/Footer'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Fragment>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Home />
+      <div className="testDiv">
+Scroll Up and Down this page to see the parallax scrolling effect.
+This div is just here to enable scrolling.
+Tip: Try to remove the background-attachment property to remove the scrolling effect.
+</div>
+<Footer />
+
       </div>
+      </Fragment>
     );
   }
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '/Users/bethanymitch/portfolio-site/portfolio/src/assets/Home.css'
+
+const Home = () => {
+    return (
+        <div class="parallax2"></div>
+    )
+}
+
+export default Home
